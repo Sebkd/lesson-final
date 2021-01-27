37a23b6e45b5872b544bfd0330820a8592a2fbfd
Sebkd <sebkd@mail.ru>
2021-01-28 00:03:06 +0700

    final-loto

--- loto-game.py
+++ loto-game.py
@@ -55,68 +55,92 @@
 Подсказка: для работы с псевдослучайными числами удобно использовать
 модуль random: http://docs.python.org/3/library/random.html
 '''
-
-from random import randint, shuffle
-
-'''создать список списков из 3 элементов по 9 значений, 5 значений это числа от 1 до 90, все 15 чисел уникальны
-12 нулей в списке списков, по 4 в списке, при этом 3 нуля с одинаковым индексом не допусается, и в списках 
-на одном индексе должен быть хоть один ноль
 '''
+Дополнение к ТЗ
+Сделать как настоящее лото
+вид карточки
+-- Карточка компьютера ---
+ 7 87     - 14    11
+      16 49    55 88    77
+   15 20     -       76  -
+--------------------------
+ 1 11 21 31 41 51 61 71 81
+            до
+10 20 30 40 50 60 70 80 90
+в колонках числа должны быть расположены как указано выше, первая колонка от 1 до 10, второй от 11 до 20 и тд
+в каждом ряду 4 пробела минимум, +3 дополнительных пробела рандомно в любом из рядов, главное чтобы в каждой 
+колонке было минимум 1 число. Бочонки гененрируются сразу и выбираются из мешка по очереди.
+'''
+from random import randint, shuffle
+from time import sleep
+
+
 class Card:
     def __init__(self, name):
         self.__row_90 = [element for element in range(1, 91)]
         self.card_row = [[], [], []]
         self.generate_card()
-        self.__name = name if len(name) else 'Vasyula'
+        self.name = name if len(name) else 'Vasyula'
+        self.counter = 15
 
-
-    def more_spaces(self, line_one, line_two, line_three):
+    @staticmethod
+    def more_spaces(line_one, line_two, line_three):
         while line_one.count(' ') < 4:
-            rand_number = randint (0, 8)
-            if (line_two[rand_number] == ' ' and line_three[rand_number] == ' '):
+            rand_number = randint(0, 8)
+            if line_two[rand_number] == ' ' and line_three[rand_number] == ' ':
                 continue
             else:
                 line_one[rand_number] = ' '
         return line_one
 
     def generate_card(self):
-        for element in range (0, 63, 7):
-            (self.card_row[0]).append (self.__row_90.pop (randint (element, (element + 9) if element < 53
+        for element in range(0, 63, 7):
+            (self.card_row[0]).append(self.__row_90.pop(randint(element, (element + 9) if element < 53
             else (element + 9))))
-            (self.card_row[1]).append (self.__row_90.pop (randint (element, (element + 8) if element < 53
+            (self.card_row[1]).append(self.__row_90.pop(randint(element, (element + 8) if element < 53
             else (element + 8))))
-            (self.card_row[2]).append (self.__row_90.pop (randint (element, (element + 7) if element < 53
+            (self.card_row[2]).append(self.__row_90.pop(randint(element, (element + 7) if element < 53
             else (element + 7))))
         '''
         раскидываем первые 9 пробелов рандомно по рядам
         '''
-        for count in range (9):
-            rand_number = randint (1, 3)
-            if (rand_number == 1 and (self.card_row[0]).count (' ') < 4):
+        for count in range(9):
+            rand_number = randint(1, 3)
+            if rand_number == 1 and (self.card_row[0]).count(' ') < 4:
                 (self.card_row[0])[count] = ' '
-            elif (rand_number == 2 and (self.card_row[1]).count (' ') < 4):
+            elif rand_number == 2 and (self.card_row[1]).count(' ') < 4:
                 (self.card_row[1])[count] = ' '
-            elif (rand_number == 3 and (self.card_row[2]).count (' ') < 4):
+            elif rand_number == 3 and (self.card_row[2]).count(' ') < 4:
                 (self.card_row[2])[count] = ' '
         '''
         Добиваем чтобы в каждом ряду было не менее 4 пробела, с условием чтобы не было рядом три пробела сверху-вниз
         '''
-        for count in range (3):
-            self.more_spaces (self.card_row[count], self.card_row[(count - 2) if count == 2 else (count + 1)],
+        for count in range(3):
+            self.more_spaces(self.card_row[count], self.card_row[(count - 2) if count == 2 else (count + 1)],
                               self.card_row[(count + 2) if count == 0 else (count - 1)])
 
     def __str__(self):
-        show_view = '-Карточка игрока ' + self.__name + '--' + '\n'
+        show_view = '-Карточка игрока ' + self.name + '--' + '\n'
         for line in self.card_row:
             answer = ''
             for element in line:
-                if len (str (element)) == 2:
-                    answer += ''.join (str (element)) + ' '
+                if len(str(element)) == 2:
+                    answer += ''.join(str(element)) + ' '
                 else:
-                    answer += ' ' + ''.join (str (element)) + ' '
+                    answer += ' ' + ''.join(str(element)) + ' '
             show_view += answer + '\n'
         show_view += '--------------------------' + '\n'
         return show_view
+
+    def get_my_value(self, value):
+        self.counter -= 1
+        value = value
+        for line in self.card_row:
+            for ind_ele, element in enumerate(line):
+                if element == value:
+                    line[ind_ele] = '\033[31m{}\033[0m'.format('X')
+                    return '\033[0m'
+
 
 class Game:
     def __init__(self, cls_one, cls_two):
@@ -124,26 +148,39 @@ class Game:
         self.gamer_two = cls_two
         self.__barrel = []
         self.generate_barrel()
-        self.start_game()
-
 
     def generate_barrel(self):
-        self.__barrel = [element for element in range (1, 91)]
+        self.__barrel = [element for element in range(1, 91)]
         shuffle(self.__barrel)
-
+       
     def start_game(self):
+
         for count in range(len(self.__barrel)):
             small_barrel = self.__barrel.pop()
-            print (f'Новый бочонок: {small_barrel} осталось {len(self.__barrel)}')
-            print (self.gamer_one)
-            print (self.gamer_two)
+            print(f'Новый бочонок: {small_barrel} осталось {len (self.__barrel)}')
+            print(self.gamer_one)
+            print(self.gamer_two)
             answer_user = input('Зачеркнуть цифру? (y/n)').lower()
-            if (answer_user == 'y') and (any(small_barrel in line for line in self.gamer_one.card_row)):
-                print ('ura')
-                return 0
+            if answer_user == 'y':
+                if any(small_barrel in line for line in self.gamer_one.card_row):
+                    self.gamer_one.get_my_value(small_barrel)
+                    if self.gamer_one.counter == 0:
+                        return print(f'Игрок {self.gamer_one.name} выйграл')
+                else:
+                    return print(f'Игрок {self.gamer_two.name} выйграл')
+            if answer_user == 'n':
+                if any(small_barrel in line for line in self.gamer_one.card_row):
+                    return print(f'Игрок {self.gamer_two.name} выйграл')
+            if any(small_barrel in line for line in self.gamer_two.card_row):
+                self.gamer_two.get_my_value(small_barrel)
+                if self.gamer_two.counter == 0:
+                    return print(f'Игрок {self.gamer_two.name} выйграл')
 
 
 card_user = Card(input('Введите ваше имя '))
 card_comp = Card('Компьютер')
 game = Game(card_user, card_comp)
-
+while True:
+    if input('Готовы начинать? ').lower() == 'y':
+        game.start_game()
+    sleep(10)
